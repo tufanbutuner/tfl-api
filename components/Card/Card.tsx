dcc032c9a54fe6d58c8f0649ed5942657969323f
tufanbutuner <tufanbutuner@gmail.com>
2022-06-01 18:02:05 +0100

    added prop to card component

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -3,12 +3,14 @@ import React, { useEffect, useState } from "react";
 
 interface CardProps {
   title?: string;
+  children?: any;
 }
 
-export default function Card({ title }: CardProps) {
+export default function Card({ title, children }: CardProps) {
   return (
     <CardContainer>
       <Title title={title}>{title}</Title>
+      {children}
     </CardContainer>
   );
 }
