eca05af323f9945cbead92e9ec619d2d2637b8db
Tufan Butuner <tufan.butuner@transformuk.com>
2022-09-04 14:03:24 +0100

    type module changes

--- sonarqube-scanner.js
+++ sonarqube-scanner.js
@@ -1,15 +1,15 @@
-const scanner = require('sonarqube-scanner');
+import scanner from "sonarqube-scanner";
 
 scanner(
   {
-  serverUrl: "http://localhost:9000",
-  login: process.env.SONAR_LOGIN,
-  password: process.env.SONAR_PASS,
-  options: {
-    "sonar.sources": "./",
-    "sonar.login": process.env.SONAR_LOGIN,
-    "sonar.password": process.env.SONAR_PASS,
+    serverUrl: "http://localhost:9000",
+    login: process.env.SONAR_LOGIN,
+    password: process.env.SONAR_PASS,
+    options: {
+      "sonar.sources": "./",
+      "sonar.login": process.env.SONAR_LOGIN,
+      "sonar.password": process.env.SONAR_PASS,
+    },
   },
-},
-() => process.exit()
+  () => process.exit()
 );
